17488a93837490d868916fb0c279ae7571f565d7
BangND <bangnd@qsoftvietnam.com>
2015-03-26 15:31:19 +0700

    update

--- list_and_dictionarie.py
+++ list_and_dictionarie.py
@@ -46,3 +46,29 @@ inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold'] = 550
 print inventory['gold']
+
+print '-------------------------------------------'
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for item in food:
+        total += food[item]
+    return total
+
+print compute_bill(prices)
