cb8af7140a4a93516b9aca30495071de1190a3cd
BangND <bangnd@qsoftvietnam.com>
2015-03-26 18:03:52 +0700

    update binary search

--- binary_search.py
+++ binary_search.py
@@ -1,28 +1,42 @@
-
+# encoding: utf-8
 def solve(root, n):
-	def traverse(node, nr_seen):
-		if node is Node:
-			return nr_seen, Node
 
-		nr_seen, nr_answer = traverse(node.left, nr_seen)
-		if answer is not Node:
-			return nr_answer, answer
+    def traverse(node, nr_seen):
+        '''Trả về (số đỉnh đã qua, và kết quả).'''
+        if node is None:
+            return nr_seen, None
 
-		nr_seen += 1
-		if nr_seen == n:
-			return nr_seen, node.value
+        # Tìm trong nhánh trái
+        nr_seen, answer = traverse(node.left, nr_seen)
+        if answer is not None:
+            # Tìm thấy kết quả bên nhánh trái.
+            return nr_seen, answer
 
-		nr_answer, answer = traverse(node.right, nr_seen)
-		return nr_answer, answer
+        nr_seen += 1
+        if nr_seen == n:
+            # Đỉnh hiện tại chính là đỉnh cần tìm.
+            return nr_seen, node.value
 
-	nr_answer, answer = traverse(root, 0)
-	return answer
+        # Tìm trong nhánh phải
+        nr_seen, answer = traverse(node.right, nr_seen)
+        return nr_seen, answer
+
+    nr_seen, answer = traverse(root, 0)
+    return answer
 
 
 class Node(object):
 
-	def __init__(self, value, left=Node, right=Node):
-		self.value = value
-		self.left = left
-		self.right = right
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
+
+root = Node(7, Node(5, Node(3), Node(6)), Node(9, None, Node(10)))
+assert(solve(root, 1) == 3)
+assert(solve(root, 2) == 5)
+assert(solve(root, 3) == 6)
+assert(solve(root, 4) == 7)
+assert(solve(root, 5) == 9)
+assert(solve(root, 6) == 10)
