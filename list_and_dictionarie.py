2584708319709fa543213376bd1ff876fe087d8a
BangND <bangnd@qsoftvietnam.com>
2015-03-26 14:19:42 +0700

    update list and dictionaries

--- list_and_dictionarie.py
+++ list_and_dictionarie.py
@@ -15,3 +15,34 @@ dog  = animals[3:6]  # The fourth through sixth characters
 print dog
 frog = animals[6:]  # From the seventh character to the end
 print frog
+
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+# Your code here!
+for number in start_list:
+    square_list.append(number ** 2)
+
+square_list.sort()
+print square_list
+
+#
+print '----------------------Lists and Dictionaries--------------------------'
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] = 550
+print inventory['gold']
