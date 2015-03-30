a8fdb9e9b39e46b9359925471372bf842cbc1334
BangND <bangnd@qsoftvietnam.com>
2015-03-30 18:16:47 +0700

    update battleship

--- battleship.py
+++ battleship.py
@@ -19,6 +19,9 @@ def print_board(board):
     for row in board:
         print " " .join(row)
 
+print "Let's play Battleship!"
+print_board(board)
+
 # Random row
 def random_row(board):
     row = randint(0, len(board) - 1)
@@ -29,6 +32,26 @@ def random_col(board):
     col = randint(0, len(board) - 1)
     return col
 
+ship_row = random_row(board)
+ship_col = random_col(board)
+
 # Input row, col
 guess_row = int(raw_input("Guess Row: "))
 guess_col = int(raw_input("Guess Col: "))
+
+print ship_row
+print ship_col
+
+if(guess_row == ship_row and guess_col == ship_col):
+    print "Congratulations! You sank my battleship!"
+else:
+    
+    if (guess_row not in range(5) or guess_col not in range(5)):
+        print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+        print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+	
+	print_board(board)
