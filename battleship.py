9ac6f9ad97593836e30f0d58c53c6dc59c1ecb50
BangND <bangnd@qsoftvietnam.com>
2015-03-30 11:00:02 +0700

    random board

--- battleship.py
+++ battleship.py
@@ -8,13 +8,23 @@ The player will have 10 guesses to try to sink the ship.
 board = []
 
 #Create a 5 x 5 grid initialized to all 'O's and store it in board.
-board = []
 for i in range(0, 5):
     board.append(["O"] * 5)
     
 #Define print_board function    
 def print_board(board):
-    for item in board:
-        print " " .join(item)
+    for row in board:
+        print " " .join(row)
 
-print_board(board)
+# Random row
+def random_row(board):
+    row = randint(0, len(board) - 1)
+    return row
+    
+# Random column
+def random_col(board):
+    col = randint(0, len(board) - 1)
+    return col
+
+random_row(board)
+random_col(board)
