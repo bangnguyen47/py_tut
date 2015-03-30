78d455e3e73aae87e1fccc42570878f1a9f95144
BangND <bangnd@qsoftvietnam.com>
2015-03-30 10:43:38 +0700

    Create a 5 x 5 grid initialized to all 'O's and store it in board.

--- battleship.py
+++ battleship.py
@@ -4,5 +4,12 @@ In this project you will build a simplified, one-player version of the classic b
 In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid.
 The player will have 10 guesses to try to sink the ship.
 """
-#Initial Board
+#Board
 board = []
+
+#Create a 5 x 5 grid initialized to all 'O's and store it in board.
+board = []
+for i in range(0, 5):
+    board.append(["O"] * 5)
+    
+print board
