d1c8b5ab1c3398bd2e156365808c626f9ace7e40
bangnd <bang.nguyen47@gmail.com>
2015-07-09 23:19:15 +0700

    example 2

---
+++ ex2.py
@@ -0,0 +1,11 @@
+__author__ = 'bangnd'
+
+#Set the following variables to the corresponding values:
+#my_int to the value 7
+#my_float to the value 1.23
+#my_bool to the value True
+
+# Set the variables to the values listed in the instructions!
+my_int = 7
+my_float = 1.23
+my_bool = True
